be7cae4e1ff6345138640e5b8b4e22978f8fd250
Shruti Deshmukh <shutideshmukh1808@gmail.com>
2026-02-19 16:40:42 +0530

    Db string updated

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,6 +1,18 @@
 import mongoose from "mongoose";
 
-export const connect_Db = async()=>{
+export const connect_Db = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.error("DB connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
-    await mongoose.connect('mongodb+srv://rdsoham5_db_user:iKYIOR6qKcF2AFXh@clusterfooddelivery.iv7bnov.mongodb.net/food-delivery').then(()=>{console.log("Db connected")})
-}
+
+
+
+
+
+
